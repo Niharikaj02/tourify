696c7d320a9ad1dc191bb18c0fcc6b3f63e20115
Niharikaj02 <jniharika1312@gmail.com>
2023-09-17 03:41:25 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import Featured from '../../components/featured/Featured';
+import MailList from '../../components/mailList/MailList';
+import PropertyList from '../../components/propertyList/PropertyList';
 import './home.css'
 
 const Home = () => {
@@ -10,8 +12,11 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured/>
+        <h1 className="homeTitle"> </h1>
+      Browse by Property Type
+      <PropertyList/>
+       <MailList/>
       </div>
-      
     </div>
   );
 };
