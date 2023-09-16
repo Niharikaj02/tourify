2c8fca638e45692186c2ad3be1d1d7fbded8aea7
Niharikaj02 <jniharika1312@gmail.com>
2023-09-17 01:37:39 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import './home.css'
-import Navbar from '../components/Navbar/Navbar'
+import Navbar from '../../components/Navbar/Navbar'
 import Header from '../components/Header/Header'
 import Featured from '../components/featured/Featured'
 
