8f2bcf992d63510d575a108eadfdb3cacb160352
Niharikaj02 <jniharika1312@gmail.com>
2023-09-17 01:32:04 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Header from '../components/Header/Header'
+import Featured from '../components/featured/Featured'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Featured />
+        <Featured/>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
 export default Home
