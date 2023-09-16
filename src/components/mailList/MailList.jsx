c11e92c5873f8889810fa8c52856e2d7b578d6fc
Niharikaj02 <jniharika1312@gmail.com>
2023-09-17 03:48:13 +0530

    commit

--- src/components/mailList/MailList.jsx
+++ src/components/mailList/MailList.jsx
@@ -2,8 +2,16 @@ import './mailList.css'
 
 const MailList = () => {
   return (
-    <div>
-      
+    <div className="mail">
+        <h1 className="mailTitle">
+            Subscribe to our mail!
+        </h1>
+        <span className='"mailDesc'>Sign up to recieve exciting offers!</span>
+        <div className="mailInputContainer">
+            <input type="text" placeholder='Your email' />
+            <button>Subscribe</button>
+            
+        </div>
     </div>
   )
 }
