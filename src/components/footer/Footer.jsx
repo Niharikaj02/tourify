410ccffa527bb75013da41af4d5f879eba2917f8
Niharikaj02 <jniharika1312@gmail.com>
2023-09-17 04:36:10 +0530

    commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,10 +11,17 @@ const Footer = () => {
             </ul>
 
             <ul className="fList">
-                <li className="fListItem">Countries</li>
-                <li className="fListItem">Regions</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">Districts</li>
+                <li className="fListItem">Apartments</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Villas</li>
+                <li className="fListItem">Hotels</li>
+                <li className="fListItem">Cabins</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Reviews</li>
+                <li className="fListItem">Deals</li>
+                <li className="fListItem">Unique places to stay</li>
+                
             </ul>
             <ul className="fList">
                 <li className="fListItem">Countries</li>
@@ -22,24 +29,7 @@ const Footer = () => {
                 <li className="fListItem">Cities</li>
                 <li className="fListItem">Districts</li>
             </ul>
-            <ul className="fList">
-                <li className="fListItem">Countries</li>
-                <li className="fListItem">Regions</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">Districts</li>
-            </ul>
-            <ul className="fList">
-                <li className="fListItem">Countries</li>
-                <li className="fListItem">Regions</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">Districts</li>
-            </ul>
-            <ul className="fList">
-                <li className="fListItem">Countries</li>
-                <li className="fListItem">Regions</li>
-                <li className="fListItem">Cities</li>
-                <li className="fListItem">Districts</li>
-            </ul>
+            
         </div>
         <div className="fText"> Copyright © 1996–2023 Booking.com™. All rights reserved.
  </div>
